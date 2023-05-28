96893f02f8e7345586576a40d4f0a39b31cd29d6
Eric Tra <tranquangduy246@gmail.com>
2023-05-28 21:45:42 +0700

    Create Item Class

---
+++ Library Management System_Duy/DuyTea/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuyTea
+{
+    // Abstract Class: Item
+    public abstract class Item
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+
+        public abstract void PrintInfo();
+    }
+}
