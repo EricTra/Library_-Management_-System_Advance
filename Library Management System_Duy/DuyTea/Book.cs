cb050207dc81a08c9b9fdb6b018be6992fba87ca
Eric Tra <tranquangduy246@gmail.com>
2023-05-28 21:48:10 +0700

    Create Book.cs

---
+++ Library Management System_Duy/DuyTea/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuyTea
+{
+    // Concrete Class: Book (Inherits from Item)
+    public class Book : Item
+    {
+        public string Genre { get; set; }
+
+        public override void PrintInfo()
+        {
+            Console.WriteLine($"Title: {Title}");
+            Console.WriteLine($"Author: {Author}");
+            Console.WriteLine($"ISBN: {ISBN}");
+            Console.WriteLine($"Genre: {Genre}");
+        }
+    }
+}
