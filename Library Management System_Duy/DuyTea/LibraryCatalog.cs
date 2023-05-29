91c6ec0fc0b47ef0b749313f944e35f2dcbdd0bd
Eric Tra <tranquangduy246@gmail.com>
2023-05-29 22:01:02 +0700

    Create LibraryCatalog.cs

---
+++ Library Management System_Duy/DuyTea/LibraryCatalog.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuyTea
+{
+    // Relationship: Binary Association
+    public class LibraryCatalog
+    {
+        private List<Item> _items;
+
+        public LibraryCatalog()
+        {
+            _items = new List<Item>();
+        }
+
+        public void AddItem(Item item)
+        {
+            _items.Add(item);
+        }
+
+        public void PrintCatalog()
+        {
+            Console.WriteLine("Library Catalog:");
+            foreach (var item in _items)
+            {
+                item.PrintInfo();
+                Console.WriteLine("--------------------");
+            }
+        }
+    }
+
+}
+
