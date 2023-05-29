ee627ce5e32e9d3a7043fed0874a3fb6ba5d74b3
Eric Tra <tranquangduy246@gmail.com>
2023-05-29 22:00:46 +0700

    Create ItemFactory.cs

---
+++ Library Management System_Duy/DuyTea/ItemFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuyTea
+{
+    // Creational Pattern: Factory Method
+    public abstract class ItemFactory
+    {
+        public abstract Item CreateItem();
+    }
+
+    public class BookFactory : ItemFactory
+    {
+        public override Item CreateItem()
+        {
+            return new Book();
+        }
+    }
+
+    public class MagazineFactory : ItemFactory
+    {
+        public override Item CreateItem()
+        {
+            return new Magazine();
+        }
+    }
+}
