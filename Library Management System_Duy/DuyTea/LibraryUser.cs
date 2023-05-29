a9029112653e97abb94c5458d1126bfdc439058d
Eric Tra <tranquangduy246@gmail.com>
2023-05-29 22:01:17 +0700

    Create LibraryUser.cs

---
+++ Library Management System_Duy/DuyTea/LibraryUser.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuyTea
+{
+    public class LibraryUser : ILibraryUserObserver
+    {
+        public string Name { get; set; }
+        public List<Item> BorrowedItems { get; private set; }
+
+        public LibraryUser(string name)
+        {
+            Name = name;
+            BorrowedItems = new List<Item>();
+        }
+
+        public void BorrowItem(Item item)
+        {
+            BorrowedItems.Add(item);
+            Console.WriteLine($"{Name} borrowed the item: {item.Title}");
+        }
+
+        public void ReturnItem(Item item)
+        {
+            BorrowedItems.Remove(item);
+            Console.WriteLine($"{Name} returned the item: {item.Title}");
+        }
+
+        public void Update(Item item)
+        {
+            Console.WriteLine($"Notification for {Name}: The item '{item.Title}' is now available.");
+        }
+    }
+
+}
