789a71dc06f248e99d639fefeb44fbb93eca1761
Eric Tra <tranquangduy246@gmail.com>
2023-06-01 12:50:45 +0700

    Create User.cs

---
+++ Library Management System_Duy/DuyTea/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuyTea
+{
+    // Abstract classes
+    abstract class User : IUser
+    {
+        protected string name;
+
+        public string GetName()
+        {
+            return name;
+        }
+    }
+}
