e8773dfa5b28f5d5a4ebf4d5175f3294d7b62496
Eric Tra <tranquangduy246@gmail.com>
2023-06-01 12:52:06 +0700

    Create LibraryItem.cs

---
+++ Library Management System_Duy/DuyTea/LibraryItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuyTea
+{
+    // Abstract classes
+    abstract class LibraryItem 
+    {
+        protected string title;
+
+        public string GetTitle()
+        {
+            return title;
+        }
+    }
+}
