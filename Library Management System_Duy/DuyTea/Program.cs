3c6f28862e7cae4f1d3948eea03f928563fee02c
Eric Tra <tranquangduy246@gmail.com>
2023-06-01 12:50:50 +0700

    Update Program.cs

--- Library Management System_Duy/DuyTea/Program.cs
+++ Library Management System_Duy/DuyTea/Program.cs
@@ -1,263 +1,142 @@
-﻿using System;
+﻿using DuyTea.DuyTea.DuyTea;
+using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-
+using System.Text;
+using System.Threading.Tasks;
 
 namespace DuyTea
 {
     // Main program
-    public class Program
+    class Program
     {
-        private static Library _library;
-        private static LibraryComposite _libraryComposite;
-        private static List<LibraryUser> _users;
-
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            _library = new Library();
-            _libraryComposite = new LibraryComposite("Library Composite");
-            _users = new List<LibraryUser>();
+            Library library = new Library();
 
-            LoadCatalogFromDataFile();
-
-            string choice;
-            do
+            while (true)
             {
-                Console.WriteLine("********** Library Management System **********");
+                Console.WriteLine("Menu:");
                 Console.WriteLine("1. Add Book");
                 Console.WriteLine("2. Add Magazine");
-                Console.WriteLine("3. Print Catalog");
-                Console.WriteLine("4. Print Library Composite");
-                Console.WriteLine("5. Borrow Item");
-                Console.WriteLine("6. Return Item");
-                Console.WriteLine("0. Exit");
+                Console.WriteLine("3. Add User");
+                Console.WriteLine("4. Borrow Item");
+                Console.WriteLine("5. Return Item");
+                Console.WriteLine("6. View Items");
+                Console.WriteLine("7. View Users");
+                Console.WriteLine("8. Exit");
+
                 Console.Write("Enter your choice: ");
-                choice = Console.ReadLine();
+                string choice = Console.ReadLine();
+                Console.WriteLine();
 
                 switch (choice)
                 {
                     case "1":
-                        AddBook();
+                        Console.Write("Enter Book Title: ");
+                        string bookTitle = Console.ReadLine();
+                        Console.Write("Enter Book Author: ");
+                        string bookAuthor = Console.ReadLine();
+
+                        library.AddItem(ItemType.Book, bookTitle, bookAuthor);
+
+                        Console.WriteLine("Book added successfully!");
+                        Console.WriteLine();
                         break;
+
                     case "2":
-                        AddMagazine();
+                        Console.Write("Enter Magazine Title: ");
+                        string magazineTitle = Console.ReadLine();
+                        Console.Write("Enter Magazine Editor: ");
+                        string magazineEditor = Console.ReadLine();
+
+                        library.AddItem(ItemType.Magazine, magazineTitle, magazineEditor);
+
+                        Console.WriteLine("Magazine added successfully!");
+                        Console.WriteLine();
                         break;
+
                     case "3":
-                        PrintCatalog();
+                        Console.Write("Enter User Name: ");
+                        string userName = Console.ReadLine();
+                        Console.Write("Enter User Address: ");
+                        string userAddress = Console.ReadLine();
+
+                        IUser user = new LibraryUser(userName, userAddress);
+                        library.AddUser(user);
+
+                        Console.WriteLine("User added successfully!");
+                        Console.WriteLine();
                         break;
+
                     case "4":
-                        PrintLibraryComposite();
-                        break;
-                    case "5":
-                        BorrowItem();
-                        break;
-                    case "6":
-                        ReturnItem();
-                        break;
-                    case "0":
-                        Console.WriteLine("Exiting Library Management System. Goodbye!");
-                        break;
-                    default:
-                        Console.WriteLine("Invalid choice. Please try again.");
-                        break;
-                }
+                        Console.Write("Enter User Name: ");
+                        string borrowerName = Console.ReadLine();
+                        Console.Write("Enter Item Title: ");
+                        string itemTitle = Console.ReadLine();
 
-                Console.WriteLine();
-            } while (choice != "0");
+                        IUser borrower = library.GetUsers().FirstOrDefault(u => u.GetName() == borrowerName);
+                        object item = library.GetItems().FirstOrDefault(i => i.GetTitle() == itemTitle);
 
-            SaveCatalogToDataFile();
-        }
-
-        public static void AddBook()
-        {
-            Console.WriteLine("\nAdding Book");
-            Console.WriteLine("Enter Book Details:");
-
-            var bookFactory = new BookFactory();
-            var book = (Book)bookFactory.CreateItem();
-
-            Console.Write("Title: ");
-            book.Title = Console.ReadLine();
-            Console.Write("Author: ");
-            book.Author = Console.ReadLine();
-            Console.Write("ISBN: ");
-            book.ISBN = Console.ReadLine();
-            Console.Write("Genre: ");
-            book.Genre = Console.ReadLine();
-
-            _library.AddItemToCatalog(book);
-            _libraryComposite.AddItem(book);
-
-            Console.WriteLine("Book added to the catalog.");
-        }
-
-        public static void AddMagazine()
-        {
-            Console.WriteLine("\nAdding Magazine");
-            Console.WriteLine("Enter Magazine Details:");
-
-            var magazineFactory = new MagazineFactory();
-            var magazine = (Magazine)magazineFactory.CreateItem();
-
-            Console.Write("Title: ");
-            magazine.Title = Console.ReadLine();
-            Console.Write("Author: ");
-            magazine.Author = Console.ReadLine();
-            Console.Write("Issue: ");
-            magazine.Issue = Console.ReadLine();
-
-            _library.AddItemToCatalog(magazine);
-            _libraryComposite.AddItem(magazine);
-
-            Console.WriteLine("Magazine added to the catalog.");
-        }
-
-        public static void PrintCatalog()
-        {
-            _library.PrintCatalog();
-        }
-
-        public static void PrintLibraryComposite()
-        {
-            _libraryComposite.PrintInfo();
-        }
-        public static void BorrowItem()
-        {
-            Console.WriteLine("\nBorrowing Item");
-            Console.WriteLine("Enter User Details:");
-
-            Console.Write("User Name: ");
-            var userName = Console.ReadLine();
-
-            var user = _users.Find(u => u.Name == userName);
-            if (user == null)
-            {
-                user = new LibraryUser(userName);
-                _users.Add(user);
-            }
-
-            Console.Write("Item Title: ");
-            var itemTitle = Console.ReadLine();
-
-            var item = _library.FindItem(itemTitle);
-
-            if (item != null)
-            {
-                _library.RemoveItemFromCatalog(item);  // Move this line after adding the item to the user's borrowed items list
-                user.BorrowItem(item);
-                Console.WriteLine("Item borrowed successfully.");
-            }
-            else
-            {
-                Console.WriteLine("Item not found in the catalog.");
-            }
-        } 
-
-        public static void ReturnItem()
-        {
-            Console.WriteLine("\nReturning Item");
-            Console.WriteLine("Enter User Details:");
-
-            Console.Write("User Name: ");
-            var userName = Console.ReadLine();
-
-            var user = _users.Find(u => u.Name == userName);
-            if (user == null)
-            {
-                Console.WriteLine("User not found.");
-                return;
-            }
-
-            Console.Write("Item Title: ");
-            var itemTitle = Console.ReadLine();
-
-            var item = user.BorrowedItems.Find(i => i.Title == itemTitle);
-            if (item != null)
-            {
-                user.ReturnItem(item);
-                _library.AddItemToCatalog(item);
-                Console.WriteLine("Item returned successfully.");
-            }
-            else
-            {
-                Console.WriteLine("Item not found in the user's borrowed items.");
-            }
-        }
-
-        public static void LoadCatalogFromDataFile()
-        {
-            if (File.Exists("catalog.txt"))
-            {
-                var lines = File.ReadAllLines("catalog.txt");
-                foreach (var line in lines)
-                {
-                    var parts = line.Split(',');
-                    if (parts.Length == 5)
-                    {
-                        var itemType = parts[0];
-                        var title = parts[1];
-                        var author = parts[2];
-                        var isbn = parts[3];
-                        var genreOrIssue = parts[4];
-
-                        Item item;
-                        if (itemType == "Book")
+                        if (borrower != null && item != null)
                         {
-                            item = new Book
-                            {
-                                Title = title,
-                                Author = author,
-                                ISBN = isbn,
-                                Genre = genreOrIssue
-                            };
-                        }
-                        else if (itemType == "Magazine")
-                        {
-                            item = new Magazine
-                            {
-                                Title = title,
-                                Author = author,
-                                Issue = genreOrIssue
-                            };
+                            LibraryUser libraryUser = (LibraryUser)borrower;
+                            libraryUser.BorrowItem(item);
+                            Console.WriteLine("Item borrowed successfully!");
                         }
                         else
                         {
-                            continue;
+                            Console.WriteLine("User or Item not found!");
                         }
 
-                        _library.AddItemToCatalog(item);
-                        _libraryComposite.AddItem(item);
-                    }
+                        Console.WriteLine();
+                        break;
+
+                    case "5":
+                        Console.Write("Enter User Name: ");
+                        string returnerName = Console.ReadLine();
+                        Console.Write("Enter Item Title: ");
+                        string returnItemTitle = Console.ReadLine();
+
+                        IUser returner = library.GetUsers().FirstOrDefault(u => u.GetName() == returnerName);
+                        object returnItem = library.GetItems().FirstOrDefault(i => i.GetTitle() == returnItemTitle);
+
+                        if (returner != null && returnItem != null)
+                        {
+                            LibraryUser libraryUser = (LibraryUser)returner;
+                            libraryUser.ReturnItem(returnItem);
+                            Console.WriteLine("Item returned successfully!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("User or Item not found!");
+                        }
+
+                        Console.WriteLine();
+                        break;
+
+                    case "6":
+                        Console.WriteLine("Library Items:");
+                        library.PrintItems();
+                        Console.WriteLine();
+                        break;
+
+                    case "7":
+                        Console.WriteLine("Library Users:");
+                        library.PrintUsers();
+                        Console.WriteLine();
+                        break;
+
+                    case "8":
+                        Console.WriteLine("Exiting...");
+                        return;
+
+                    default:
+                        Console.WriteLine("Invalid choice! Please try again.");
+                        Console.WriteLine();
+                        break;
                 }
             }
-        }
-
-        public static void SaveCatalogToDataFile()
-        {
-            var lines = new List<string>();
-            foreach (var item in _library.LibraryItems)
-            {
-                var itemType = item.GetType().Name;
-                var title = item.Title;
-                var author = item.Author;
-
-                if (itemType == "Book")
-                {
-                    var book = (Book)item;
-                    var isbn = book.ISBN;
-                    var genre = book.Genre;
-                    lines.Add($"{itemType},{title},{author},{isbn},{genre}");
-                }
-                else if (itemType == "Magazine")
-                {
-                    var magazine = (Magazine)item;
-                    var issue = magazine.Issue;
-                    lines.Add($"{itemType},{title},{author},{issue}");
-                }
-            }
-
-            File.WriteAllLines("catalog.txt", lines);
         }
     }
 }
