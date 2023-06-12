16ab789e7dbb01ebde22b20fcd79d6721602d10c
Trà Quang Duy <tranquangduy246@gmail.com>
2023-06-12 15:08:59 +0700

    Create Item.cs

---
+++ Library Management System_Duy/DuyTea/Item.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuyTea
+{
+    // Item class
+    public abstract class Item
+    {
+        public string title;
+
+        public string GetTitle()
+        {
+            return title;
+        }
+
+        public abstract void TemplateItemInfo();
+
+        public abstract void PrintInfo();
+
+        public abstract void UpdateInfo(string newTitle, int newYearOfPublication);
+    }
+}
