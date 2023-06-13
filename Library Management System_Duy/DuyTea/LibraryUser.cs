e2a939a8c0b3ed59f38a7e14cda1cd2814b8e653
Trà Quang Duy <tranquangduy246@gmail.com>
2023-06-13 11:43:54 +0700

    Update LibraryUser.cs

--- Library Management System_Duy/DuyTea/LibraryUser.cs
+++ Library Management System_Duy/DuyTea/LibraryUser.cs
@@ -6,24 +6,40 @@ using System.Threading.Tasks;
 
 namespace DuyTea
 {
+    // Class representing a library user
     public class LibraryUser
     {
-        public string address;
-        public List<Item> borrowedItem;
+        private string address;
+        private List<Item> borrowedItems;
+
+        public LibraryUser(string address)
+        {
+            this.address = address;
+            borrowedItems = new List<Item>();
+        }
 
         public void BorrowItem(Item item)
         {
-            borrowedItem.Add(item);
+            borrowedItems.Add(item);
+            Console.WriteLine("Item '" + item.GetTitle() + "' borrowed successfully.");
         }
 
         public void ReturnItem(Item item)
         {
-            borrowedItem.Remove(item);
+            if (borrowedItems.Contains(item))
+            {
+                borrowedItems.Remove(item);
+                Console.WriteLine("Item '" + item.GetTitle() + "' returned successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Item not found in user's borrowed items.");
+            }
         }
 
         public override string ToString()
         {
-            return $"Address: {address}";
+            return "Address: " + address;
         }
     }
 }
